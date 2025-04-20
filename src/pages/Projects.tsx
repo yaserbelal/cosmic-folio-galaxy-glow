3262a7d6e123b6015fe6f85bab72d4386c59ce6f
yaserbelal <yasserbelal2005@gmail.com>
2025-04-20 12:38:48 +0200

    edit 2

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -25,7 +25,12 @@ const Projects = () => {
   }, []);
   
   const projects: ProjectType[] = [
-    
+    {
+      title: "Employee-Management-System",
+      description: "A comprehensive web-based application for user management and database interaction. This project was developed as part of the GDG On Campus Zagazig Tasks. It utilizes PHP for server-side scripting and integrates with a MySQL database, showcasing core principles of web development, including form handling, data validation, and CRUD operations. Through this project, we gained practical experience in backend development, database design, and secure data processing — essential skills in backend web development.",
+      tags: ["PHP","Javascript","Database"],
+      github: "https://github.com/yaserbelal/Employee-Management-System",
+    },
     {
       title: "Quran-Playlist-Manager",
       description: "A comprehensive console-based application for managing playlists. This project was developed as part of the 2024 Data Structures course at Zagazig University. It leverages a custom-built Double linked list data structure, showcasing a range of object-oriented programming (OOP) principles. This project provided hands-on experience with data structure design, algorithm efficiency, and OOP concepts, allowing for both learning and practical application of these key software engineering skills.",
