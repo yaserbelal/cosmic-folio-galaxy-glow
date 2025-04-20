3d94e2417f30330ae7e5e126e703d55cdbf4c5fe
yaserbelal <yasserbelal2005@gmail.com>
2025-04-20 12:33:56 +0200

    edit 1

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -25,6 +25,7 @@ const Projects = () => {
   }, []);
   
   const projects: ProjectType[] = [
+    
     {
       title: "Quran-Playlist-Manager",
       description: "A comprehensive console-based application for managing playlists. This project was developed as part of the 2024 Data Structures course at Zagazig University. It leverages a custom-built Double linked list data structure, showcasing a range of object-oriented programming (OOP) principles. This project provided hands-on experience with data structure design, algorithm efficiency, and OOP concepts, allowing for both learning and practical application of these key software engineering skills.",
